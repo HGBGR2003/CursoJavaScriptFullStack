c2ef4e3c056dfbf2d0c72a9510e4f324ebd174f7
Henrique Gabriel <henriquegb152019@gmail.com>
2025-10-16 14:11:30 -0300

    Organizando

---
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/aula-10/index.js
@@ -0,0 +1,11 @@
+let nome = "Henrique"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+// nome = window.prompt("Insira seu Nome:")
+// let confirmacao = window.confirm("Confirma essa ação?")
+
+
