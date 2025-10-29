2bcff0506a9716a92d4e2f93193b5b7e2f672b3c
Henrique Gabriel <henriquegb152019@gmail.com>
2025-10-29 17:42:31 -0300

    Exercicio 1 feito da aula 17

--- Modulo 4  - JS (JavaScript)/js-modulo-1/exercicios-fixacao/exercicios-aula-17/exercicio-velocidade-veiculos/index.js
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/exercicios-fixacao/exercicios-aula-17/exercicio-velocidade-veiculos/index.js
@@ -1,1 +1,20 @@
-console.log("Continuo depois")
+let primeiroVeiculo
+let segundoVeiculo
+let velocidadePrimeiro
+let velocidadeSegundo
+
+primeiroVeiculo = prompt("Informe o nome do primeiro veiculo:")
+
+velocidadePrimeiro = parseFloat(prompt("Informe a velocidade do primeiro veiculo:"))
+
+segundoVeiculo = prompt("Informe o nome do segundo veiculo:")
+
+velocidadeSegundo = parseFloat(prompt("Informe a velocidade do segundo veiculo:"))
+
+if (velocidadePrimeiro > velocidadeSegundo) {
+    console.log("O mais rápido é o primeiro carro.")
+}else if (velocidadeSegundo > velocidadePrimeiro) {
+    console.log("O mais rápido é o segundo veículo.")
+}else if(velocidadePrimeiro == velocidadeSegundo){
+    console.log("Os dois tiveram a mesma velocidade.")
+}
