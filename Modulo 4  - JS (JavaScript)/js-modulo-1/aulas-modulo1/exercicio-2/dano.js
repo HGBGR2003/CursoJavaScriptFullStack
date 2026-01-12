4ee10da1671805e26f06e97fed4e16b795488f35
Henrique Gabriel <henriquegb152019@gmail.com>
2026-01-12 11:01:57 -0300

    Retomando correcao do exercicio 2

---
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/exercicio-2/dano.js
@@ -0,0 +1,10 @@
+const atacante = prompt("Qual é o nome do personagem atacante?")
+const poderDeAtaque = parseFloat(prompt("Qual é o seu poder de ataque?"))
+
+const defensor = prompt("Qual é o nome do personagem defensor?")
+let pontosDeVida = parseFloat(prompt("Quantos pontos de vida ela possui?"))
+const poderDeDefesa = parseFloat(prompt("Qual é o seu poder de defesa?"))
+const possuiEscudo = prompt("Ele possui um escudo? (Sim/Não)")
+
+let danoCausado = 0
+
