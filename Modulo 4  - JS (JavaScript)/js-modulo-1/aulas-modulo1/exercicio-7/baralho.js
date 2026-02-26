6bd9b2f55c840ef4171d9e3ba6ea0cc15adc7eb5
Henrique Gabriel <henriquegb152019@gmail.com>
2026-02-26 14:29:47 -0300

    Resolvendo exercicios da aaula 35

---
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/exercicio-7/baralho.js
@@ -0,0 +1,34 @@
+const baralho = []
+let opcao = ""
+
+do {
+    
+    opcao = prompt(
+        "Cartas no baralho: " + baralho.length + 
+        "\n1. Adicionar uma carta\n2. Puxar uma carta\n3. Sair"
+    )
+
+    switch (opcao) {
+        case "1":
+            const novaCarta = prompt("Qual é a carta?")
+            baralho.push(novaCarta)
+            break
+
+        case "2":
+            const cartaPuxada = baralho.pop()
+            if (cartaPuxada) {
+                console.log("Já volto....")
+            }
+
+            break
+
+        case "3":
+            alert("Saindo....")
+            break
+    
+        default:
+            alert("Opção Inválida!")
+            break;
+    }
+
+} while (opcao !== "3")
