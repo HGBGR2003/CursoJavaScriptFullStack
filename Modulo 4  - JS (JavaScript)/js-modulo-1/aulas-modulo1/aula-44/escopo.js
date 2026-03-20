8f917aa2fc4863d7441c5c8d0395befaf3209f6b
Henrique Gabriel <henriquegb152019@gmail.com>
2026-03-20 14:25:16 -0300

    Aula Terminada

--- Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/aula-44/escopo.js
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/aula-44/escopo.js
@@ -1,24 +1,8 @@
-let pokemon = "Charmander"
+console.log(nome)
+// console.log(sobrenome)
 
-function evoluir() {
-    pokemon = "Charmeleon"
-}
+var nome = "Henrique"
+let sobrenome = "Pontes"
 
-console.log(pokemon)
-evoluir()
-console.log(pokemon)
-
-function criarAnimal() {
-    let animal = "Gato"
-    // return animal // <= Esse seria o jeito para evitar que o valor saia vazio, e não encontre a variavel.
-}
-
-// console.log(animal)
-
-function avaliarNota(nota) {
-    if (condition) {
-        
-    } else {
-        
-    }
-}
+console.log(nome)
+// console.log(sobrenome)
