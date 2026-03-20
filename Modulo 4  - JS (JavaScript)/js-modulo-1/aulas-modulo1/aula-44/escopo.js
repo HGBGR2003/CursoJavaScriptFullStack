000c4aafc5a592a1f2669bdc38ebe190b39e2e78
Henrique Gabriel <henriquegb152019@gmail.com>
2026-03-20 14:07:29 -0300

    Aula sobre escopo de variavies.

---
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/aula-44/escopo.js
@@ -0,0 +1,24 @@
+let pokemon = "Charmander"
+
+function evoluir() {
+    pokemon = "Charmeleon"
+}
+
+console.log(pokemon)
+evoluir()
+console.log(pokemon)
+
+function criarAnimal() {
+    let animal = "Gato"
+    // return animal // <= Esse seria o jeito para evitar que o valor saia vazio, e não encontre a variavel.
+}
+
+// console.log(animal)
+
+function avaliarNota(nota) {
+    if (condition) {
+        
+    } else {
+        
+    }
+}
