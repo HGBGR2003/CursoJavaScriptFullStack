430fd2da67d21adbed4ae6848f600c8e66f8e5ce
Henrique Gabriel <henriquegb152019@gmail.com>
2026-04-02 16:47:40 -0300

    Funcoes de Ordem Altas, aula

---
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/aula-50/hof.js
@@ -0,0 +1,18 @@
+function calcular(a,b, operacao){
+    console.log("Realizando uma operação. ")
+    const resultado = operacao(a,b)
+    return resultado
+}
+
+function somar(x, y) {
+    console.log("Realizando uma soma.")
+    return x + y
+}
+
+ console.log(calcular(3, 5, somar))
+
+ console.log(calcular(8, 4, function(x, y){
+    console.log("Realizando uma subtração.")
+    return x - y
+}))
+ 
