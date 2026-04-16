bdfbef813b148b2cff0af323774305696bc29404
Henrique Gabriel <henriquegb152019@gmail.com>
2026-04-16 13:19:31 -0300

    Arrumado.

--- Projetos - Por Modulo/Projeto - Módulo 4 (JavaScript)/script.js
+++ Projetos - Por Modulo/Projeto - Módulo 4 (JavaScript)/script.js
@@ -7,13 +7,25 @@ function novaVaga() {
   let descricao = prompt("Descrição da vaga:");
   let dataLimite = prompt("Informe a data limite para a vaga continuar");
 
-  lista.push({
-    indice: lista.length + 1,
-    nomeDaVaga: novaVaga,
-    descricao: descricao,
-    dataLimite: dataLimite,
-    candidatos: [],
-  });
+  if (
+    confirm(
+      "Confirmar nova vaga?\n\nNome: " +
+        novaVaga +
+        "\nDescrição: " +
+        descricao +
+        "\nData limite: " +
+        dataLimite,
+    )
+  ) {
+    lista.push({
+      indice: lista.length + 1,
+      nomeDaVaga: novaVaga,
+      descricao: descricao,
+      dataLimite: dataLimite,
+      candidatos: [],
+    });
+    alert("Vaga criada com sucesso!");
+  }
 }
 
 function visualizarVaga() {
@@ -21,7 +33,10 @@ function visualizarVaga() {
     let indice = parseInt(prompt("Índice da vaga:")) - 1;
     let vagaConsultada = lista[indice];
     alert(
-      `Nome: ${vagaConsultada.nomeDaVaga}\nDescrição: ${vagaConsultada.descricao}`,
+      "Nome: " +
+        vagaConsultada.nomeDaVaga +
+        "\nDescrição: " +
+        vagaConsultada.descricao,
     );
   } else {
     alert("Não tem vagas disponiveis.");
@@ -41,9 +56,16 @@ function incresver() {
 function excluirVaga() {
   let indice = parseInt(prompt("Informe o índice da vaga:")) - 1;
   let vaga = lista[indice];
-  
+
   alert(
-    `Nome: ${vaga.nomeDaVaga}\nDescrição: ${vaga.descricao}\nData limite: ${vaga.dataLimite}\nCandidatos: ${vaga.candidatos.length}`,
+    "Nome: " +
+      vaga.nomeDaVaga +
+      "\nDescrição: " +
+      vaga.descricao +
+      "\nData limite: " +
+      vaga.dataLimite +
+      "\nCandidatos: " +
+      vaga.candidatos.length,
   );
 
   if (confirm("Confirma a exclusão da vaga?")) {
@@ -53,56 +75,59 @@ function excluirVaga() {
 }
 
 function principal() {
-  listaDePessoas = "";
-
-  for (let i = 0; i < lista.length; i++) {
-    listaDePessoas +=
-      i +
-      1 +
-      "º " +
-      lista[i].nomeDaVaga +
-      " | Candidatos: " +
-      lista[i].candidatos.length +
-      "\n";
-  }
-
-  nomeOperacao = prompt(
-    "Vagas de Emprego\n" +
-      (listaDePessoas || "Nenhuma vaga disponivel.") +
-      "\n\nEscolha uma opção:\n" +
-      "1 - Criar um nova vaga\n" +
-      "2 - Visualizar uma vaga\n" +
-      "3 - Inscrever um canditado em uma vaga\n" +
-      "4 - Exclir uma vaga\n" +
-      "5 - Sair",
-  );
+  let nomeOperacao;
 
   do {
-    switch (nomeOperacao) {
-        case "1":
-            novaVaga()
-            break;
+    listaDePessoas = "";
 
-        case "2":
-            visualizarVaga()
-            break
-        
-        case "3":
-            incresver()
-            break
-            
-        case "4":
-            excluirVaga()
-            break
-        
-        case "5":
-            alert("Saindo....")
-            break
-
-        default:
-            alert("Opção Não Encontrada tente de novo.")
-            break;
+    for (let i = 0; i < lista.length; i++) {
+      listaDePessoas +=
+        i +
+        1 +
+        "º " +
+        lista[i].nomeDaVaga +
+        " | Candidatos: " +
+        lista[i].candidatos.length +
+        "\n";
     }
 
+    nomeOperacao = prompt(
+      "Vagas de Emprego\n" +
+        (listaDePessoas || "Nenhuma vaga disponivel.") +
+        "\n\nEscolha uma opção:\n" +
+        "1 - Criar um nova vaga\n" +
+        "2 - Visualizar uma vaga\n" +
+        "3 - Inscrever um canditado em uma vaga\n" +
+        "4 - Exclir uma vaga\n" +
+        "5 - Sair",
+    );
+
+    switch (nomeOperacao) {
+      case "1":
+        novaVaga();
+        break;
+
+      case "2":
+        visualizarVaga();
+        break;
+
+      case "3":
+        incresver();
+        break;
+
+      case "4":
+        excluirVaga();
+        break;
+
+      case "5":
+        alert("Saindo....");
+        break;
+
+      default:
+        alert("Opção Não Encontrada tente de novo.");
+        break;
+    }
   } while (nomeOperacao != "5");
 }
+
+principal();
