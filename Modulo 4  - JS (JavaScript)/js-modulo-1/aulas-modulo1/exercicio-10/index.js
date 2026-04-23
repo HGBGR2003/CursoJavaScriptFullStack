8371d7b42634bdd7e8909bb4463d04bedcd84755
Henrique Gabriel <henriquegb152019@gmail.com>
2026-04-23 13:13:58 -0300

    Criando funcoes

--- Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/exercicio-10/index.js
+++ Modulo 4  - JS (JavaScript)/js-modulo-1/aulas-modulo1/exercicio-10/index.js
@@ -1,18 +1,79 @@
-const vagas = []
+const vagas = [];
 
-function listarVagas(){
-    const vagasEmTexto = vagas.reduce(function (textoFinal, vaga, indice){
-        textoFinal += indice + ". "
-        textoFinal += vaga.nome
-        textoFinal += " (" + vaga.canditados.length + " canditados)\n"
-        return textoFinal
-    }, "")
+function listarVagas() {
+  const vagasEmTexto = vagas.reduce(function (textoFinal, vaga, indice) {
+    textoFinal += indice + ". ";
+    textoFinal += vaga.nome;
+    textoFinal += " (" + vaga.canditados.length + " canditados)\n";
+    return textoFinal;
+  }, "");
 
-    alert(vagasEmTexto)
+  alert(vagasEmTexto);
 }
 
-function novaVaga(){
-    const nome
-    const descricao
-    const dataLimite
+function novaVaga() {
+  const nome = prompt("Informe um nome para a vaga:");
+  const descricao = prompt("Informe uma descrição para a vaga:");
+  const dataLimite = prompt(
+    "Informe uma data limite (dd/mm/aaaa) para a vaga:.",
+  );
+
+  const confirmacao = confirm(
+    "Deseja criar uma nova vaga com esssas informações?\n" +
+      "Nome: " +
+      nome +
+      "\nDescrição: " +
+      descricao +
+      "\nData Limite: " +
+      dataLimite,
+  );
+
+  if (confirmacao) {
+    const novaVaga = {
+      nome,
+      descricao,
+      dataLimite,
+      canditados: [],
+    };
+
+    vagas.push(novaVaga);
+    alert("Vaga criada.");
+  }
+}
+
+function exibirVaga() {
+  const indice = prompt("Informe o índice da vaga que deseja exibir:");
+  const vaga = vagas[indice];
+
+  const candidatosEmTexto = vaga.canditados.reduce(function (
+    textoFinal,
+    candidato,
+  ) {
+    return textoFinal + "\n - " + candidato;
+  }, "");
+
+  alert(
+    "Vaga nº " +
+      indice +
+      "\nNome: " +
+      vaga.nome +
+      "\nDescrição: " +
+      vaga.descricao +
+      "\nData Limite: " +
+      vaga.dataLimite +
+      "\nQuantidade de candidatos: " +
+      vaga.candidatos.length +
+      "\nCandidatos inscritos: " +
+      candidatosEmTexto,
+  );
+}
+
+function inscreverCandidato() {
+    const canditado = prompt("Informe o nome do (a) candidato(a):")
+    const indice = prompt("Informe o índice da vaga para qual o(a) candidato(a) deseja se inscrever:")
+    const vaga = vagas[indice]
+
+    const confirmacao = confirm(
+        "Deseja inscrever o candidato " + canditado
+    )
 }
