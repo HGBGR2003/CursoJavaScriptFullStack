0112e34027e85412056581abb63861af21442982
Henrique Gabriel <henriquegb152019@gmail.com>
2026-05-07 14:32:12 -0300

    Aula de Criacao de elementos no html, usando javascript

--- Módulo 4  - JS (JavaScript)/js-modulo-2-doom/aulas-modulo2/05-criando-elementos/index.js
+++ Módulo 4  - JS (JavaScript)/js-modulo-2-doom/aulas-modulo2/05-criando-elementos/index.js
@@ -1,4 +1,27 @@
-function addInput(){
-    const ul = document.getElementById('inputs')
-    // Continuo amanhã.
+function addInput() {
+  const ul = document.getElementById("inputs");
+  const newLi = document.createElement("li");
+
+  newLi.className = "list-item";
+  newLi.innerText = "Novo input: ";
+
+  const newInput = document.createElement("input");
+  newInput.type = "text";
+  newInput.type = "text";
+  newInput.name = "input";
+
+  newLi.appendChild(newInput); //<= Ordem de Hierarquia
+  // <li>
+  //    <input type= "type" name="input">
+  // </li>
+  ul.appendChild(newLi); //<= Ordem de Hierarquia
+  /*
+    
+    <ul>
+        <li>
+            <input type= "type" name="input">
+        </li>
+    </ul>
+
+  */
 }
