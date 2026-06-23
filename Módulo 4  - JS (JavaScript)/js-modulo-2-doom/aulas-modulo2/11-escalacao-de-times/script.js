8a663f09ecf78f6adea33c8a27b2e882361a098f
Henrique Gabriel <henriquegb152019@gmail.com>
2026-06-23 13:01:50 -0300

    Atualizando

---
+++ Módulo 4  - JS (JavaScript)/js-modulo-2-doom/aulas-modulo2/11-escalacao-de-times/script.js
@@ -0,0 +1,36 @@
+function addPlayer() {
+  const position = document.getElementById("position").value;
+  const name = document.getElementById("name").value;
+  const number = document.getElementById("number").value;
+
+  //console.log({positon, name, number}) <= Aparece como objeto.
+
+  const confirmation = confirm("Escalar" + name + " como " + position + "?");
+
+  if (confirmation) {
+    const teamList = document.getElementById("team-list");
+    const playerItem = document.createElement("li");
+
+    playerItem.id = "player-" + number;
+    playerItem.innerText = position + ": " + name + " (" + number + ")";
+    teamList.appendChild(playerItem);
+
+    document.getElementById("position").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("number").value = ""; 
+  }
+}
+
+function removePlayer() {
+  const number = document.getElementById("numberToRemove").value;
+  const playerToRemove = document.getElementById("player-" + number);
+
+  const confirmation = confirm(
+    "Remover o Jogador " + playerToRemove.innerText + "?",
+  );
+
+  if (confirmation) {
+    playerToRemove.remove()
+    document.getElementById("numberToRemove").value = "";
+  }
+}
